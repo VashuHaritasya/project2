8e12b1cfa95859d38fcaa530397ea78e000fcdd9
Vashu Haritasya <vashusharma246@gmail.com>
2022-10-12 13:50:02 +0530

    name change

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
-let searchForm = document.querySelector('.header .search-form');
+let searchForm = document.querySelector(".header .search-form");
 
-document.querySelector('#search-btn').onclick = () => {
-  searchForm.classList.toggle('active');
-  navbar.classList.remove('active');
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
 };
 
-let navbar = document.querySelector('.header .navbar');
+let navbar = document.querySelector(".header .navbar");
 
-document.querySelector('#menu-btn').onclick = () => {
-  navbar.classList.toggle('active');
-  searchForm.classList.remove('active');
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
 };
 
 window.onscroll = () => {
-  searchForm.classList.remove('active');
-  navbar.classList.remove('active');
+  searchForm.classList.remove("active");
+  navbar.classList.remove("active");
 };
 
-let slides = document.querySelectorAll('.home .slide');
+let slides = document.querySelectorAll(".home .slide");
 let index = 0;
 
 function next() {
-  slides[index].classList.remove('active');
+  slides[index].classList.remove("active");
   index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
+  slides[index].classList.add("active");
 }
 
 function prev() {
-  slides[index].classList.remove('active');
+  slides[index].classList.remove("active");
   index = (index - 1 + slides.length) % slides.length;
-  slides[index].classList.add('active');
+  slides[index].classList.add("active");
+}
+
+function loadCoupon() {
+  document.getElementById("coupon").style.visibility = "visible";
+  document.getElementById("main").style.opacity = "0.7";
+}
+
+function closeCoupon() {
+  document.getElementById("coupon").style.visibility = "hidden";
+  document.getElementById("main").style.opacity = "1";
 }
